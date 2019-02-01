479060b173cc0e7339865a4f843a5e2ae72278a4
kkheon <kkheon@gmail.com>
2019-02-01 12:44:09 +0900

    'main_yuv_diff_framework' is updated.   - plot [ORG, LR, LR+HM, LR+HM+UP, ORG+HM]

--- yuv_diff/main_yuv_diff_framework.py
+++ yuv_diff/main_yuv_diff_framework.py
@@ -3,14 +3,20 @@ import os
 import glob
 import re
 import numpy as np
+import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot
 
 from yuv_diff import yuv_diff
 from yuv_diff import yuv_diff_temporal
+from yuv_diff import yuv_diff_n_frame
+
 from utils import plot_psnr_diff
 from utils import plot_ssim_diff
+from utils import plot_framework_diff
 from utils import parse_dec_bit_lcu
+
+from read_yuv import read_yuv420
 
 import math
 
@@ -19,33 +25,44 @@ if __name__ == '__main__':
     # settings
     label_path = "./data_vsr/val/label"
     input_path = "./data_vsr/val/label"
-    output_path = "./result_diff_temporal"
+    output_path = "./result_diff_framework"
 
-    #w = 3840
-    #h = 2160
     w = 1920
-    #h = 1072
     h = 1080
 
+    w_up = 3840
+    h_up = 2160
+
     block_size = 64
-    #block_size = 128
-    #block_size = 256
-    scale = 1
+    block_size_up = 128
+
+    w_up_in_block = math.ceil(w_up / block_size)
+    h_up_in_block = math.ceil(h_up / block_size)
+
+    scale = 2
 
     start_frame = 0
     frame_size = 5
 
 
+    # input list
+    # input 1 : label, High Resolution(HR)
+    list_label = ['/home/kkheon/dataset/myanmar_v1_15frm/orig/scenes_yuv/val/scene_53.yuv']
+    # input 2 : HR's HM result
+    list_label_rec = ['/home/kkheon/dataset/myanmar_v1/orig_hm/val/QP32/rec_scene_53.yuv']
+    # input 3 : LR
+    list_down = ['/home/kkheon/VSR-Tensorflow-exp-mf1/data_vsr/val_mf1/result_QP32/result_mf_vcnn_down_3/mf_vcnn_down_scene_53.yuv']
+    # input 4 : LR's HM result
+    list_down_rec = ['/data/kkheon/data_vsr_bak/val/val_t1_mf1_vcnn_fixed_ipppp/result_QP32/result_mf_vcnn_down_3_hm/QP32/rec_mf_vcnn_down_scene_53.yuv']
+    # input 5 : LR's HM result + up
+    list_down_rec_up = ['/data/kkheon/data_vsr_bak/val/val_t1_mf1_vcnn_fixed_ipppp/result_QP32/result_mf_vcnn_up_4/QP32/mf_vcnn_up_rec_mf_vcnn_down_scene_53.yuv']
+
     ## path setting
     #list_label = sorted(glob.glob(os.path.join(label_path, "*.yuv")))
     #list_input = sorted(glob.glob(os.path.join(input_path, "*.yuv")))
 
-
     # hm result
     #list_input = ['/home/kkheon/HM-16.9_CNN/bin/data_vsr/test/label_hm_arcnn/QP32/rec_BasketballDrive.yuv']
-
-    # down-sampling data
-    list_input = ['/home/kkheon/VSR-Tensorflow-exp-mf1/data_vsr/val_mf1/result_QP32/result_mf_vcnn_down_3/mf_vcnn_down_scene_53.yuv']
 
     # bit info
     #list_dec_bit_lcu = ['/home/kkheon/scripts/yuv_diff/dec/decoder_bit_lcu.txt']
@@ -55,67 +72,127 @@ if __name__ == '__main__':
         os.makedirs(output_path)
 
     # save each image's PSNR result as file.
-    for idx, each_image in enumerate(list_input):
-        each_input = list_input[idx]
+    for idx, each_image in enumerate(list_down_rec):
 
-        list_df_psnr, list_df_ssim, input_y = yuv_diff_temporal(each_input, start_frame, w, h, frame_size, block_size, scale)
+        # load LR
+        each_down_yuv = list_down_rec[idx]
+        array_input_y, array_input_cbcr = read_yuv420(each_down_yuv, w, h, frame_size, start_frame=start_frame)
+        input_y_down = array_input_y.squeeze(axis=3)
 
-        # get bitrate info
+        # load LR+HM
+        each_down_yuv = list_down_rec[idx]
+        array_input_y, array_input_cbcr = read_yuv420(each_down_yuv, w, h, frame_size, start_frame=start_frame)
+        input_y_down_rec = array_input_y.squeeze(axis=3)
+
+        # diff label-HM
+        each_label = list_label[idx]
+        each_label_rec = list_label_rec[idx]
+        df_psnr_label_rec, df_sse_label_rec, label_y, input_y_label_rec = yuv_diff_n_frame(each_label, start_frame, each_label_rec, start_frame, w_up, h_up, block_size_up, scale, frame_size)
+
+        # diff label-LR-UP
+        each_down_rec_up = list_down_rec_up[idx]
+        df_psnr_down_rec_up, df_sse_down_rec_up, label_y, input_y_down_rec_up = yuv_diff_n_frame(each_label, start_frame, each_down_rec_up, start_frame, w_up, h_up, block_size_up, scale, frame_size)
+
+        # get HR bitrate info
+        bit_filename = '/home/kkheon/scripts/yuv_diff/dec/dec_hr/decoder_bit_lcu.txt'
+        df_bit_lcu_hr = parse_dec_bit_lcu(bit_filename)
+
+        # TODO : need a transform to down-sampled sized bit
+        # sum 2x2 value into 1
+
+        # get LR bitrate info
         # assumption : bit info file is already generated.
-        bit_filename = '/home/kkheon/scripts/yuv_diff/dec/decoder_bit_lcu.txt'
-        df_dec_bit_lcu = parse_dec_bit_lcu(bit_filename)
+        bit_filename = '/home/kkheon/scripts/yuv_diff/dec/dec_lr/decoder_bit_lcu.txt'
+        df_bit_lcu_lr = parse_dec_bit_lcu(bit_filename)
 
         # save block-image
         w_in_block = int(w / block_size)
         for y in range(0, h, block_size):
             for x in range(0, w, block_size):
+                # block index
+                y_up = int(y * 2)
+                x_up = int(x * 2)
 
                 # block index
                 y_in_block = int(y / block_size)
                 x_in_block = int(x / block_size)
 
-                psnrs = [None]  # first frame have no difference.
-                ssims = [None]  # first frame have no difference.
-
                 # get psnr, ssim value
-                for each_frame_index in range(0, frame_size - 1):
-                    # psnr
-                    each_df_psnr = list_df_psnr[each_frame_index]
-                    psnr = each_df_psnr.iloc[y_in_block][x_in_block]
-                    psnrs.append(psnr)
-
-                    # ssim
-                    each_df_ssim = list_df_ssim[each_frame_index]
-                    ssim = each_df_ssim.iloc[y_in_block][x_in_block]
-                    ssims.append(ssim)
-
-                bitrates = []
                 for each_frame_index in range(0, frame_size):
-                    # bitrate
-                    each_df_bitrate = df_dec_bit_lcu.loc[df_dec_bit_lcu['frame'] == each_frame_index]
+                    psnrs = [None, None, None]  # label, lr, enc(lr)
+                    ssims = [None, None, None]
+
+                    # psnr : LR-HM-UP
+                    each_df_psnr_down_rec_up = df_psnr_down_rec_up.loc[df_psnr_down_rec_up['frame'] == each_frame_index]
+                    each_block_psnr_down_rec_up = each_df_psnr_down_rec_up.iloc[y_in_block][x_in_block]
+                    psnrs.append(each_block_psnr_down_rec_up)
+
+                    # psnr : label-HM
+                    each_df_psnr_label_rec = df_psnr_label_rec.loc[df_psnr_label_rec['frame'] == each_frame_index]
+                    each_block_psnr_label_rec = each_df_psnr_label_rec.iloc[y_in_block][x_in_block]
+                    psnrs.append(each_block_psnr_label_rec)
+
+                    bitrates = [None, None]
+
+                    # bitrate : LR-HM
+                    each_df_bitrate = df_bit_lcu_lr.loc[df_bit_lcu_lr['frame'] == each_frame_index]
                     block_index = y_in_block * w_in_block + x_in_block
                     bitrate = each_df_bitrate.iloc[block_index]
                     bitrates.append(bitrate['bit'])
 
-                result_imgs = []
-                xlabels = []
-                # pick block from input
-                for each_frame_index in range(0, frame_size):
-                    result_imgs.append(input_y[each_frame_index, y:y+block_size, x:x+block_size])
-                    xlabels.append(start_frame + each_frame_index)
+                    # add again for LR+HM+UP
+                    bitrates.append(bitrate['bit'])
 
-                # separate dir depending on PSNR
-                psnr_diff = np.mean(psnrs[1:])
-                if math.isinf(psnr_diff):
-                    psnr_diff = 99.0
-                psnr_diff_floor = math.floor(psnr_diff)
-                str_psnr_diff_floor = str(psnr_diff_floor)
-                output_path_psnr = os.path.join(output_path, 'group_psnr_' + str_psnr_diff_floor)
+                    # sum 2x2 block's bits
+                    # bitrate : label+HM
+                    # Step 1 : extract frame's bit info
+                    each_df_bitrate = df_bit_lcu_hr.loc[df_bit_lcu_hr['frame'] == each_frame_index]
 
-                if not os.path.exists(output_path_psnr):
-                    os.makedirs(output_path_psnr)
+                    # Step 2 : extract bits from df & reshape values
+                    each_df_bitrate_reshaped = pd.DataFrame(each_df_bitrate['bit'].values.reshape(h_up_in_block, w_up_in_block))
 
-                #plot_psnr_diff(result_imgs, psnrs, idx, x, y, save_dir=output_path_psnr)
-                plot_ssim_diff(result_imgs, psnrs, ssims, bitrates, xlabels, idx, x, y, save_dir=output_path_psnr)
+                    # Step 3 : 2x2 sum
+                    each_df_bitrate_reshaped[-2] = each_df_bitrate_reshaped.index / 2
+                    each_df_bitrate_reshaped[-2] = each_df_bitrate_reshaped[-2].astype('int')
+                    each_df_bitrate_reshaped = each_df_bitrate_reshaped.groupby(-2).sum()
+                    each_df_bitrate_reshaped = each_df_bitrate_reshaped.transpose()
+                    each_df_bitrate_reshaped[-2] = each_df_bitrate_reshaped.index / 2
+                    each_df_bitrate_reshaped[-2] = each_df_bitrate_reshaped[-2].astype('int')
+                    each_df_bitrate_reshaped = each_df_bitrate_reshaped.groupby(-2).sum()
+                    each_df_bitrate_reshaped = each_df_bitrate_reshaped.transpose()
+
+                    # Step 4 : add to list
+                    bitrate = each_df_bitrate_reshaped.iloc[y_in_block][x_in_block]
+                    bitrates.append(bitrate)
+
+                    #block_index = y_in_block * w_in_block + x_in_block
+                    #bitrate = each_df_bitrate.iloc[block_index]
+                    #bitrates.append(bitrate['bit'])
+
+                    # plot single frame
+                    xlabels = ['ORG', 'LR', 'LR+HM', 'LR+HM+UP', 'ORG+HM']
+                    result_imgs = []
+
+                    # append imgs
+                    result_imgs.append(label_y[each_frame_index, y_up:y_up+block_size_up, x_up:x_up+block_size_up])
+                    result_imgs.append(input_y_down[each_frame_index, y:y+block_size, x:x+block_size])
+                    result_imgs.append(input_y_down_rec[each_frame_index, y:y+block_size, x:x+block_size])
+                    result_imgs.append(input_y_down_rec_up[each_frame_index, y_up:y_up+block_size_up, x_up:x_up+block_size_up])
+                    result_imgs.append(input_y_label_rec[each_frame_index, y_up:y_up+block_size_up, x_up:x_up+block_size_up])
+
+                    # separate dir depending on PSNR
+                    psnr_diff = psnrs[3] - psnrs[4]  # psnr(LR+HM+UP) - psnr(HM)
+
+                    if math.isinf(psnr_diff):
+                        psnr_diff = 99.0
+                    psnr_diff_floor = math.floor(psnr_diff)
+                    str_psnr_diff_floor = str(psnr_diff_floor)
+                    output_path_psnr = os.path.join(output_path, 'group_psnr_' + str_psnr_diff_floor)
+
+                    if not os.path.exists(output_path_psnr):
+                        os.makedirs(output_path_psnr)
+
+                    #plot_psnr_diff(result_imgs, psnrs, idx, x, y, save_dir=output_path_psnr)
+                    plot_framework_diff(result_imgs, psnrs, bitrates, xlabels, idx, x, y, save_dir=output_path_psnr)
 
 
--- yuv_diff/yuv_diff.py
+++ yuv_diff/yuv_diff.py
@@ -77,6 +77,90 @@ def yuv_diff(filename_label, start_frame_label, filename_input, start_frame_inpu
 
     return df_psnr, df_sse, label_y_pel, input_y_pel
 
+def yuv_diff_n_frame(filename_label, start_frame_label, filename_input, start_frame_input, w, h, block_size, scale, n_frame):
+
+    print("========== FRM : label=%03d, input=%03d ==========\n" % (start_frame_label, start_frame_input))
+
+    # read yuv : label
+    array_label_y, array_label_cbcr = read_yuv420(filename_label, w, h, n_frame, start_frame=start_frame_label)
+    label_y = array_label_y.squeeze(axis=3)
+
+    # read yuv : input
+    array_input_y, array_input_cbcr = read_yuv420(filename_input, w, h, n_frame, start_frame=start_frame_input)
+    input_y = array_input_y.squeeze(axis=3)
+
+    # calculate MSE in pixel domain
+    # make a copy of pixel domain
+    label_y_pel = label_y.copy()
+    input_y_pel = input_y.copy()
+
+    # normalization
+    label_y = label_y / 255.
+    input_y = input_y / 255.
+
+    df_psnr = pd.DataFrame()
+    df_sse = pd.DataFrame()
+
+    for frame_index in range(0, n_frame):
+        # count for each frame
+        block_count = 0
+
+        # loop based on block
+        list_psnr_frame = []
+        list_sse_frame = []
+        for y in range(0, h, block_size):
+            list_psnr_row = []
+            list_sse_row = []
+
+            for x in range(0, w, block_size):
+
+                # pick block from input
+                sub_label_y = label_y[frame_index, y:y+block_size, x:x+block_size]
+                sub_input_y = input_y[frame_index, y:y+block_size, x:x+block_size]
+
+                # PSNR calculation
+                each_psnr = psnr(sub_input_y, sub_label_y, scale)
+                #each_ssim = ssim(sub_input_y, sub_label_y, data_range=256)
+
+                list_psnr_row.append(float("{0:.4f}".format(each_psnr)))
+
+                # TODO : later
+                # yuv diff
+                # save diff as image
+                # combine yuv
+
+                # calculate SSE in pixel domain
+                sub_label_y_pel = label_y_pel[frame_index, y:y+block_size, x:x+block_size]
+                sub_input_y_pel = input_y_pel[frame_index, y:y+block_size, x:x+block_size]
+
+                # SSE
+                each_sse = np.sum((sub_label_y_pel - sub_input_y_pel) ** 2)
+                list_sse_row.append(float("{0:.4f}".format(each_sse)))
+
+                block_count += 1
+
+            list_psnr_frame.append(list_psnr_row)
+            list_sse_frame.append(list_sse_row)
+
+        # outside of the loop-block
+
+        # stat the block-level PSNR
+        # list_psnr to df
+        df_psnr_frm = pd.DataFrame(list_psnr_frame)
+        df_sse_frm = pd.DataFrame(list_sse_frame)
+
+        # add frame column
+        df_psnr_frm.insert(0, 'frame', start_frame_input+frame_index)
+        df_sse_frm.insert(0, 'frame', start_frame_input+frame_index)
+
+        # append to global table.
+        df_psnr = df_psnr.append(df_psnr_frm)
+        df_sse = df_sse.append(df_sse_frm)
+
+        # CHECKME : # why don't you add each block info to main data frame? no need to transform list to df.
+
+    return df_psnr, df_sse, label_y_pel, input_y_pel
+
 
 def yuv_diff_temporal(filename_input, start_frame_input, w, h, frame_size, block_size, scale):
 
